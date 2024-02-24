ba449d6531f95e99989978f84852124524b2ea3c
suraj a <surajra0628@gmail.com>
2024-02-24 10:47:26 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@
       
       </header>
       <div className="px-20  pl-[25%] ">
-        <h1 className="text-9xl w-full">THE FESTIVAL </h1><span className="text-9xl w-full pl-10">OF URGENT </span><h1 className="text-9xl w-full">REINVENTION</h1>
+        <h1 className="text-9xl w-full font-bold">THE FESTIVAL </h1><span className="text-9xl w-full pl-10 font-bold">OF URGENT </span><h1 className="text-9xl w-full font-bold">REINVENTION</h1>
+      </div>
+      <div className="pt-10">
+         <div className="flex justify-center items-center gap-16">
+          <h6>WHEN</h6>
+
+          <h6>MISSED THE FESTIVAL ?</h6>
+        </div>
+      </div>
+
+      <div className="pt-10">
+         <div className="flex justify-center items-center gap-16">
+          <h6>Feb 3-4 ,2022</h6>
+
+          <h6>Watch Event</h6>
+        </div>
+
+         
       </div>
     </main>
   )
