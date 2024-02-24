c44ab65fdca94dbb3991c0c03fa6ce86cf864f84
suraj a <surajra0628@gmail.com>
2024-02-24 15:13:49 +0530

    added hover

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,28 @@ const App =()=>{
     <main className="relative">
       <Nav />
       <section className=''><Hero /></section>
-      <section>
+      {/* BUTTONS */}
+      <section className="flex padding-x gap-10 pt-10">
+      <button className='h-16 w-[20rem] bg-slate-300 rounded-3xl hover:bg-blue-400'>
+        </button>
+
+        <button className='h-16 w-[20rem] bg-slate-300 rounded-3xl hover:bg-red-400'>
+        </button>
+
+        <button className='h-16 w-[20rem] bg-slate-300 rounded-3xl hover:bg-green-400'>
+        </button>
+
+        <button className='h-16 w-[20rem] bg-slate-300 rounded-3xl hover:bg-yellow-400 '>
+        </button>
+
+
+
+        {/* <Buttons />
         <Buttons />
+        <Buttons /> */}
       </section>
-      <section className="gap-2 flex  overflow-x-auto">
+      {/*  */}
+      <section className=" flex  overflow-x-auto padding-x">
        <Card />
        <Card />
        <Card />
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Buttons = () => {
   return (
-    <div>Buttons</div>
+    <div className=' pt-10'>
+        <button className='h-16 w-[20rem] bg-slate-300 rounded-3xl hover:bg-blue-400'>
+        </button>
+    </div>
   )
 }
 
