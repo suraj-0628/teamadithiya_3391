78ec876ae4d2f18540ec92226260bf9f1b2468fa
suraj a <surajra0628@gmail.com>
2024-02-24 10:37:45 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@
   return (
     <main className="relative w-full h-full">
       <header className="">
-
+      
       </header>
+      <div className="px-20  pl-[25%] ">
+        <h1 className="text-9xl w-full">THE FESTIVAL </h1><span className="text-9xl w-full pl-10">OF URGENT </span><h1 className="text-9xl w-full">REINVENTION</h1>
+      </div>
     </main>
   )
  }
