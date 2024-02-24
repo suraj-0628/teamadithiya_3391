4e92687e314b061f181fba09c3cab4e5ac7c13f7
suraj a <surajra0628@gmail.com>
2024-02-24 11:08:58 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,24 @@
           <h6 className="text-2xl font-semibold">Feb 3-4 ,2022</h6>
           <h6 className="text-2xl font-semibold underline">Watch Event</h6>
         </div>
+      </div>
+      {/* come back here  */}
+      <div className="pt-10 pl-[25%] ">
+        <div className="flex justify-start items-center gap-32 border bg-black text-white w-[30%] px-6 p-3 rounded-3xl">
+          <h1 className="underline">Day One</h1>
+          <p>THU ,FEB 3</p>
+        </div>
+      </div>
 
-         
+      {/*  */}
+      <div className="pt-5 pl-[25%]">
+        <div className="flex justify-start items-center gap-32">
+          <p>9:05am -9:10am PST</p>
+          <div>
+          <h1 className="text-xl font-semibold ">Welcome</h1>
+          <p className="font-light">PRESENTED BY KARP YOUMAN ,EMCEE</p>
+          </div>
+        </div>
       </div>
     </main>
   )
